e96198cbfcc0a64d4e94dae631f680b32e7d6d75
t-creates <geislinger@pm.me>
2023-02-05 14:08:06 -0700

    Movie Buff

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect } from 'react';
+import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+import { useGetGenresQuery } from '../../services/TMDB';
+import useStyles from './styles';
+import genreIcons from '../../assets/genres';
+
+const categories = [
+  { label: 'Popular', value: 'popular' },
+  { label: 'Top Rated', value: 'top_rated' },
+  { label: 'Upcoming', value: 'upcoming' },
+];
+
+const Sidebar = ({ setMobileOpen }) => {
+  const { genreIdOrCategoryName } = useSelector((state) => state.currentGenreOrCategory);
+  const classes = useStyles();
+  const { data, isFetching } = useGetGenresQuery();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setMobileOpen(false);
+  }, [genreIdOrCategoryName]);
+
+  return (
+    <>
+      <Link to="/" className={classes.imageLink}>
+        <h1 className={classes.image}>
+          MOVIE BUFF
+        </h1>
+      </Link>
+      <Divider />
+      <List>
+        <ListSubheader>Categories</ListSubheader>
+        {categories.map(({ label, value }) => (
+          <Link key={value} className={classes.links} to="/">
+            <ListItem onClick={() => dispatch(selectGenreOrCategory(value))} button>
+              <ListItemIcon>
+                <img src={genreIcons[label.toLowerCase()]} className={classes.genreImage} height={30} />
+              </ListItemIcon>
+              <ListItemText primary={label} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        <ListSubheader>Genres</ListSubheader>
+        {isFetching ? (
+          <Box display="flex" justifyContent="center">
+            <CircularProgress />
+          </Box>
+        ) : data.genres.map(({ name, id }) => (
+          <Link key={name} className={classes.links} to="/">
+            <ListItem onClick={() => dispatch(selectGenreOrCategory(id))} button>
+              <ListItemIcon>
+                <img src={genreIcons[name.toLowerCase()]} className={classes.genreImage} height={30} />
+              </ListItemIcon>
+              <ListItemText primary={name} />
+            </ListItem>
+          </Link>
+        ))}
+      </List>
+    </>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/Sidebar/styles.js
@@ -0,0 +1,21 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles((theme) => ({
+  imageLink: {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '10% 0',
+  },
+  image: {
+    width: '70%',
+    font: 'bold 2.5rem/1.5em "Press Start 2P", oswald, cursive, sans-serif',
+    color: theme.palette.text.primary,
+  },
+  links: {
+    color: theme.palette.text.primary,
+    textDecoration: 'none',
+  },
+  genreImage: {
+    filter: theme.palette.mode === 'dark' && 'invert(1)',
+  },
+}));
